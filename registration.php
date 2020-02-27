810a7e56f2e96ad6dcdfdf4c3b9b3bd57ed291fc
hampus.westman <hampus.westman@lybe.se>
2020-02-27 21:11:30 +0100

    stable release. can parse dotenv

---
+++ registration.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Hampus Westman 2020
+ * See LICENCE provided with this module for licence details
+ *
+ * @author     Hampus Westman <hampus.westman@gmail.com>
+ * @copyright  Copyright (c) 2020 Hampus Westman
+ * @license    MIT License https://opensource.org/licenses/MIT
+ * @link       https://github.com/Pr00xxy
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PrOOxxy_DotEnv',
+    __DIR__
+);
